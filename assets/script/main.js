01cba5f70529bcec41e592976f161d3ad18ba778
monica <monibrave1@gmail.com>
2023-03-27 23:29:27 -0300

    add sructure app vue.js

---
+++ assets/script/main.js
@@ -0,0 +1,24 @@
+
+const { createApp } = Vue //desestructuring
+const app = createApp({
+    data() {
+        return {
+        message: 'hello world'
+        }
+    },
+    created(){
+
+    },
+    mounted() {
+
+    },
+    methods: {
+
+    },
+    computed:{
+
+    }
+    
+}).mount('#app');
+
+
